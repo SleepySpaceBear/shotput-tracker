1cf5bde8a2d6389364e7827cf2da5f66ddd01cfc
Gabriel Alexander <sleepyspacedad@gmail.com>
2020-04-11 00:05:56 -0400

    defined App::OnExit()

--- src/app.cpp
+++ src/app.cpp
@@ -2,11 +2,14 @@
 
 #include "ui.hpp"
 
-
 bool App::OnInit() {
 	UI *frame = new UI("Shotput Tracker", wxPoint(50, 50), wxSize(450, 340));
 	frame->Show(true);
 	return true;
+}
+
+int App::OnExit() {
+	return 0;
 }
 
 wxIMPLEMENT_APP(App);
