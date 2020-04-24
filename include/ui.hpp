d1b0c6087d9744690aee80a379610d31e3c1c5f7
Gabriel Alexander <sleepyspacedad@gmail.com>
2020-04-24 13:36:54 -0400

    Added Load Video stub to UI

--- include/ui.hpp
+++ include/ui.hpp
@@ -2,14 +2,20 @@
 #define UI_HPP
 #include "wx/wx.h"
 
+/*
+ * Unscoped enum for UI events 
+*/
+enum : int { EV_LOAD_VIDEO = 0, NUM_EVENTS };
+
 class App;
 
 class UI : public wxFrame {
 public:
 	UI(const wxString& title, const wxPoint& pos, const wxSize& size);
 private:
-	void OnExit(wxCommandEvent& event);
+	void OnLoadVideo(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
 
 	wxDECLARE_EVENT_TABLE();
 };
--- src/ui.cpp
+++ src/ui.cpp
@@ -4,7 +4,10 @@
 
 UI::UI(const wxString& title, const wxPoint& pos, const wxSize& size)
 	: wxFrame(NULL, wxID_ANY, title, pos, size) {
+	
 	wxMenu *menuFile = new wxMenu;
+	wxMenuItem* loadVideo = new wxMenuItem(menuFile, EV_LOAD_VIDEO, "Load Video", "Select a video to load");
+	menuFile->Append(loadVideo);
 	menuFile->AppendSeparator();
 	menuFile->Append(wxID_EXIT);
 	wxMenu *menuHelp = new wxMenu;
@@ -15,8 +18,8 @@ UI::UI(const wxString& title, const wxPoint& pos, const wxSize& size)
 	SetMenuBar(menuBar);
 }
 
-void UI::OnExit(wxCommandEvent & event) {
-	Close(true);
+void UI::OnLoadVideo(wxCommandEvent& event) {
+
 }
 
 void UI::OnAbout(wxCommandEvent & event) {
@@ -24,7 +27,12 @@ void UI::OnAbout(wxCommandEvent & event) {
 		"About Shotput Tracker", wxOK | wxICON_INFORMATION);
 }
 
+void UI::OnExit(wxCommandEvent & event) {
+	Close(true);
+}
+
 wxBEGIN_EVENT_TABLE(UI, wxFrame)
-EVT_MENU(wxID_EXIT, UI::OnExit)
+EVT_MENU(EV_LOAD_VIDEO, UI::OnLoadVideo)
 EVT_MENU(wxID_ABOUT, UI::OnAbout)
+EVT_MENU(wxID_EXIT, UI::OnExit)
 wxEND_EVENT_TABLE();
